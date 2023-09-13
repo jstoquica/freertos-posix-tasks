007bc954b6ae3677171a5913275632c4efd5117a
Kauany2 <116275430+Kauany2@users.noreply.github.com>
2023-09-13 09:50:03 -0300

    aula presente

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 // Task handles for task management
 TaskHandle_t Task1_Handle;
 TaskHandle_t Task2_Handle;
+TaskHandle_t Task3_Handle;
 
 // Function that represents the first task
 void Task1(void *pvParameters) {
@@ -37,11 +38,22 @@ void Task2(void *pvParameters) {
     }
 }
 
+void Task3(void *pvParameters) {
+    for (;;)
+    {
+        printf("Task 3 is running...\n");
+        vTaskDelete(Task1_Handle);
+        vTaskDelay(1000 / portTICK_PERIOD_MS); // Delay for 2 seconds
+        xTaskCreate(StopTask, "StopTask", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
+    }
+}
+
 
 int main(void) {
     // Initialize the FreeRTOS kernel
     xTaskCreate(Task1, "Task1", configMINIMAL_STACK_SIZE, NULL, 1, &Task1_Handle);
     xTaskCreate(Task2, "Task2", configMINIMAL_STACK_SIZE, NULL, 1, &Task2_Handle);
+    xTaskCreate(Task3, "Task3", configMINIMAL_STACK_SIZE, NULL, 1, &Task3_Handle);
 
     // Start the FreeRTOS scheduler
     vTaskStartScheduler();
