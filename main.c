51a316fa0c652c1b57fa1767ba3bdb08ebf968fb
JoseDavi10 <116275071+JoseDavi10@users.noreply.github.com>
2023-09-13 21:51:46 -0300

    modify task freertos

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 // Task handles for task management
 TaskHandle_t Task1_Handle;
 TaskHandle_t Task2_Handle;
+TaskHandle_t Task3_Handle;
 
 // Function that represents the first task
 void Task1(void *pvParameters) {
@@ -12,6 +13,14 @@ void Task1(void *pvParameters) {
     {
         printf("Task 1 is running...\n");
         vTaskDelay(1000 / portTICK_PERIOD_MS); // Delay for 1 second
+    }
+}
+
+void Task3(void *pvParameters) {
+    for (;;)
+    {
+        printf("Rodando 1 e 2, pagando amabas...\n");
+        vTaskDelay(2000 / portTICK_PERIOD_MS); // Delay for 1 second
     }
 }
 
@@ -37,11 +46,11 @@ void Task2(void *pvParameters) {
     }
 }
 
-
 int main(void) {
     // Initialize the FreeRTOS kernel
     xTaskCreate(Task1, "Task1", configMINIMAL_STACK_SIZE, NULL, 1, &Task1_Handle);
     xTaskCreate(Task2, "Task2", configMINIMAL_STACK_SIZE, NULL, 1, &Task2_Handle);
+    xTaskCreate(Task3, "Task3", configMINIMAL_STACK_SIZE, NULL, 1, &Task3_Handle);
 
     // Start the FreeRTOS scheduler
     vTaskStartScheduler();
